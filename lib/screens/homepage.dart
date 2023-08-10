57082382c9de292abaaf77d718e2873c35feb446
Barend Koorzen <koorzenb@gmail.com>
2023-08-10 09:26:09 +0200

    Added ListView

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -10,6 +10,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late Map<String, Map<String, dynamic>> items;
+
+  @override
+  void initState() {
+    setState(() {
+      items = {
+        "Item1": {'description': 'item1 name', 'price': 12.99},
+        "Item2": {'description': 'item2 name', 'price': 5.99},
+      };
+      super.initState();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +33,19 @@ class _HomePageState extends State<HomePage> {
         // foregroundColor: Colors.white,
         titleTextStyle: Theme.of(context).textTheme.titleLarge,
       ),
-      body: const Center(
-        child: Text('nothing here'),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          String itemKey = items.keys.elementAt(index);
+          Map<String, dynamic> item = items[itemKey]!;
+          final description = item['description'];
+          final price = item['price']; // TODO: convert this all to a stless widget
+          return ListTile(
+            leading: const Icon(Icons.image),
+            title: Text(description),
+            trailing: Text(price.toString()),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async => await Get.to(() => const CaptureItemPage()),

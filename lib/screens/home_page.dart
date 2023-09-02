5e9a8818ba64516bd748ccb73584af636d1c5cea
Barend Koorzen <koorzenb@gmail.com>
2023-09-02 12:02:14 +0200

    Renamed home_page.dart

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,6 +13,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    // HomeScreen(),
+    CaptureItemPage(),
+    CheckoutScreen(),
+  ];
   late Map<String, Map<String, dynamic>> items;
 
   @override

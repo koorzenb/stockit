22df616681a8f767e8c7ef1d8aa142d53ed89898
Barend Koorzen <koorzenb@gmail.com>
2023-09-07 04:38:48 +0200

    Added radioListTiles to checkout

--- lib/screens/checkout_page.dart
+++ lib/screens/checkout_page.dart
@@ -1,12 +1,32 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class CheckoutPage extends StatelessWidget {
   const CheckoutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Checkout something...'),
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RadioListTile(
+          value: true,
+          groupValue: 'bank',
+          onChanged: null,
+          title: Text('ABSA'),
+        ),
+        RadioListTile(
+          value: true,
+          groupValue: 'bank',
+          onChanged: null,
+          title: Text('Capitec'),
+        ),
+        RadioListTile(
+          value: true,
+          groupValue: 'bank',
+          onChanged: null,
+          title: Text('FNB'),
+        ),
+      ],
     );
   }
 }
